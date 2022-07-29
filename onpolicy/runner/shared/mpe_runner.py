ae4ce44e1bb2466cd031ad7e180d4c36b3f084b0
Sihong He <sihong.he@uconn.edu>
2022-07-28 22:07:19 -0400

    add collect_noise and eval_noise funcs

--- onpolicy/runner/shared/mpe_runner.py
+++ onpolicy/runner/shared/mpe_runner.py
@@ -113,6 +113,38 @@ class MPERunner(Runner):
         self.buffer.obs[0] = obs.copy()
 
     @torch.no_grad()
+    def collect_noise(self, step):
+        self.trainer.prep_rollout()
+        share_obs_noise = self.addNoise(np.concatenate(self.buffer.share_obs[step]))
+        obs_noise = self.addNoise(np.concatenate(self.buffer.obs[step]))
+        value, action, action_log_prob, rnn_states, rnn_states_critic \
+            = self.trainer.policy.get_actions(share_obs_noise,
+                            obs_noise,
+                            np.concatenate(self.buffer.rnn_states[step]),
+                            np.concatenate(self.buffer.rnn_states_critic[step]),
+                            np.concatenate(self.buffer.masks[step]))
+        # [self.envs, agents, dim]
+        values = np.array(np.split(_t2n(value), self.n_rollout_threads))
+        actions = np.array(np.split(_t2n(action), self.n_rollout_threads))
+        action_log_probs = np.array(np.split(_t2n(action_log_prob), self.n_rollout_threads))
+        rnn_states = np.array(np.split(_t2n(rnn_states), self.n_rollout_threads))
+        rnn_states_critic = np.array(np.split(_t2n(rnn_states_critic), self.n_rollout_threads))
+        # rearrange action
+        if self.envs.action_space[0].__class__.__name__ == 'MultiDiscrete':
+            for i in range(self.envs.action_space[0].shape):
+                uc_actions_env = np.eye(self.envs.action_space[0].high[i] + 1)[actions[:, :, i]]
+                if i == 0:
+                    actions_env = uc_actions_env
+                else:
+                    actions_env = np.concatenate((actions_env, uc_actions_env), axis=2)
+        elif self.envs.action_space[0].__class__.__name__ == 'Discrete':
+            actions_env = np.squeeze(np.eye(self.envs.action_space[0].n)[actions], 2)
+        else:
+            raise NotImplementedError
+
+        return values, actions, action_log_probs, rnn_states, rnn_states_critic, actions_env
+        
+    @torch.no_grad()
     def collect(self, step):
         self.trainer.prep_rollout()
         value, action, action_log_prob, rnn_states, rnn_states_critic \
@@ -169,6 +201,51 @@ class MPERunner(Runner):
         for eval_step in range(self.episode_length):
             self.trainer.prep_rollout()
             eval_action, eval_rnn_states = self.trainer.policy.act(np.concatenate(eval_obs),
+                                                np.concatenate(eval_rnn_states),
+                                                np.concatenate(eval_masks),
+                                                deterministic=True)
+            eval_actions = np.array(np.split(_t2n(eval_action), self.n_eval_rollout_threads))
+            eval_rnn_states = np.array(np.split(_t2n(eval_rnn_states), self.n_eval_rollout_threads))
+            
+            if self.eval_envs.action_space[0].__class__.__name__ == 'MultiDiscrete':
+                for i in range(self.eval_envs.action_space[0].shape):
+                    eval_uc_actions_env = np.eye(self.eval_envs.action_space[0].high[i]+1)[eval_actions[:, :, i]]
+                    if i == 0:
+                        eval_actions_env = eval_uc_actions_env
+                    else:
+                        eval_actions_env = np.concatenate((eval_actions_env, eval_uc_actions_env), axis=2)
+            elif self.eval_envs.action_space[0].__class__.__name__ == 'Discrete':
+                eval_actions_env = np.squeeze(np.eye(self.eval_envs.action_space[0].n)[eval_actions], 2)
+            else:
+                raise NotImplementedError
+
+            # Obser reward and next obs
+            eval_obs, eval_rewards, eval_dones, eval_infos = self.eval_envs.step(eval_actions_env)
+            eval_episode_rewards.append(eval_rewards)
+
+            eval_rnn_states[eval_dones == True] = np.zeros(((eval_dones == True).sum(), self.recurrent_N, self.hidden_size), dtype=np.float32)
+            eval_masks = np.ones((self.n_eval_rollout_threads, self.num_agents, 1), dtype=np.float32)
+            eval_masks[eval_dones == True] = np.zeros(((eval_dones == True).sum(), 1), dtype=np.float32)
+
+        eval_episode_rewards = np.array(eval_episode_rewards)
+        eval_env_infos = {}
+        eval_env_infos['eval_average_episode_rewards'] = np.sum(np.array(eval_episode_rewards), axis=0)
+        eval_average_episode_rewards = np.mean(eval_env_infos['eval_average_episode_rewards'])
+        print("eval average episode rewards of agent: " + str(eval_average_episode_rewards))
+        self.log_env(eval_env_infos, total_num_steps)
+    
+    @torch.no_grad()
+    def eval_noise(self, total_num_steps):
+        eval_episode_rewards = []
+        eval_obs = self.eval_envs.reset()
+
+        eval_rnn_states = np.zeros((self.n_eval_rollout_threads, *self.buffer.rnn_states.shape[2:]), dtype=np.float32)
+        eval_masks = np.ones((self.n_eval_rollout_threads, self.num_agents, 1), dtype=np.float32)
+
+        for eval_step in range(self.episode_length):
+            self.trainer.prep_rollout()
+            eval_obs_noise = self.addNoise(np.concatenate(eval_obs))
+            eval_action, eval_rnn_states = self.trainer.policy.act(eval_obs_noise,
                                                 np.concatenate(eval_rnn_states),
                                                 np.concatenate(eval_masks),
                                                 deterministic=True)
