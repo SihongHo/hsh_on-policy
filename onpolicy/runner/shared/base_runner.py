0ee73f952da274ab0c9175a9e1dafc6ed6ff7b8d
Sihong He <sihong.he@uconn.edu>
2022-07-28 23:42:19 -0400

    add noise_std para

--- onpolicy/runner/shared/base_runner.py
+++ onpolicy/runner/shared/base_runner.py
@@ -40,6 +40,7 @@ class Runner(object):
         self.use_wandb = self.all_args.use_wandb
         self.use_render = self.all_args.use_render
         self.recurrent_N = self.all_args.recurrent_N
+        self.noise_std = self.all_args.noise_std
 
         # interval
         self.save_interval = self.all_args.save_interval
