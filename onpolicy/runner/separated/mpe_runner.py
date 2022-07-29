39eeaee0072fad3ab0d94df60e6f9d5261587679
Sihong He <sihong.he@uconn.edu>
2022-07-28 21:53:16 -0400

    test addnoise func

--- onpolicy/runner/separated/mpe_runner.py
+++ onpolicy/runner/separated/mpe_runner.py
@@ -9,6 +9,13 @@ import torch
 from onpolicy.utils.util import update_linear_schedule
 from onpolicy.runner.separated.base_runner import Runner
 import imageio
+import scipy.stats as stats
+
+def TruncatedNormal(mean = 0.0, std = 1.0, threshold = 1.0):
+    lower, upper = -threshold, threshold
+    mu, sigma = mean, std
+    X = stats.truncnorm((lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
+    return X
 
 def _t2n(x):
     return x.detach().cpu().numpy()
@@ -16,40 +23,109 @@ def _t2n(x):
 class MPERunner(Runner):
     def __init__(self, config):
         super(MPERunner, self).__init__(config)
-       
+        if self.noise_std == 0.0:
+            self.X = None
+        else:
+            self.X = TruncatedNormal(std = self.noise_std)
+
+    def addNoise(self, origin_obs):
+        if self.noise_std == 0.0:
+            return origin_obs
+        obs_array = origin_obs
+        noise = self.X.rvs(np.size(obs_array))
+        noise.shape = obs_array.shape
+        obs_array = obs_array + noise
+        return obs_array  
+
     def run(self):
         self.warmup()   
 
         start = time.time()
         episodes = int(self.num_env_steps) // self.episode_length // self.n_rollout_threads
 
-        for episode in range(episodes):
+        # for episode in range(episodes * 0.75): # 3/4 training
+        #     if self.use_linear_lr_decay:
+        #         for agent_id in range(self.num_agents):
+        #             self.trainer[agent_id].policy.lr_decay(episode, episodes)
+
+        #     for step in range(self.episode_length):
+        #         # Sample actions
+        #         values, actions, action_log_probs, rnn_states, rnn_states_critic, actions_env = self.collect(step)
+                    
+        #         # Obser reward and next obs
+        #         obs, rewards, dones, infos = self.envs.step(actions_env)
+
+        #         data = obs, rewards, dones, infos, values, actions, action_log_probs, rnn_states, rnn_states_critic 
+                
+        #         # insert data into buffer
+        #         self.insert(data)
+
+        #     # compute return and update network
+        #     self.compute()
+        #     train_infos = self.train()
+            
+        #     # post process
+        #     total_num_steps = (episode + 1) * self.episode_length * self.n_rollout_threads
+            
+        #     # save model
+        #     if (episode % self.save_interval == 0 or episode == episodes - 1):
+        #         self.save()
+
+        #     # log information
+        #     if episode % self.log_interval == 0:
+        #         end = time.time()
+        #         print("\n Scenario {} Algo {} Exp {} updates {}/{} episodes, total num timesteps {}/{}, FPS {}.\n"
+        #                 .format(self.all_args.scenario_name,
+        #                         self.algorithm_name,
+        #                         self.experiment_name,
+        #                         episode,
+        #                         episodes,
+        #                         total_num_steps,
+        #                         self.num_env_steps,
+        #                         int(total_num_steps / (end - start))))
+
+        #         if self.env_name == "MPE":
+        #             for agent_id in range(self.num_agents):
+        #                 idv_rews = []
+        #                 for info in infos:
+        #                     if 'individual_reward' in info[agent_id].keys():
+        #                         idv_rews.append(info[agent_id]['individual_reward'])
+        #                 train_infos[agent_id].update({'individual_rewards': np.mean(idv_rews)})
+        #                 train_infos[agent_id].update({"average_episode_rewards": np.mean(self.buffer[agent_id].rewards) * self.episode_length})
+        #         self.log_train(train_infos, total_num_steps)
+
+        #     # eval
+        #     if episode % self.eval_interval == 0 and self.use_eval:
+        #         self.eval(total_num_steps)
+
+        for episode in range(episodes * 0.75, episodes): # 1/4 testing
             if self.use_linear_lr_decay:
                 for agent_id in range(self.num_agents):
                     self.trainer[agent_id].policy.lr_decay(episode, episodes)
 
             for step in range(self.episode_length):
                 # Sample actions
-                values, actions, action_log_probs, rnn_states, rnn_states_critic, actions_env = self.collect(step)
+                values, actions, action_log_probs, rnn_states, rnn_states_critic, actions_env = self.collect_noise(step)
                     
                 # Obser reward and next obs
                 obs, rewards, dones, infos = self.envs.step(actions_env)
 
-                data = obs, rewards, dones, infos, values, actions, action_log_probs, rnn_states, rnn_states_critic 
+                # data = obs, rewards, dones, infos, values, actions, action_log_probs, rnn_states, rnn_states_critic 
                 
                 # insert data into buffer
-                self.insert(data)
+                # self.insert(data)
 
             # compute return and update network
-            self.compute()
-            train_infos = self.train()
+            # self.compute()
+            # train_infos = self.train()
+            train_infos = self.without_train_get_inform()
             
             # post process
             total_num_steps = (episode + 1) * self.episode_length * self.n_rollout_threads
             
             # save model
-            if (episode % self.save_interval == 0 or episode == episodes - 1):
-                self.save()
+            # if (episode % self.save_interval == 0 or episode == episodes - 1):
+                # self.save()
 
             # log information
             if episode % self.log_interval == 0:
@@ -76,7 +152,9 @@ class MPERunner(Runner):
 
             # eval
             if episode % self.eval_interval == 0 and self.use_eval:
-                self.eval(total_num_steps)
+                self.eval_noise(total_num_steps)
+
+
 
     def warmup(self):
         # reset env
@@ -107,6 +185,63 @@ class MPERunner(Runner):
             value, action, action_log_prob, rnn_state, rnn_state_critic \
                 = self.trainer[agent_id].policy.get_actions(self.buffer[agent_id].share_obs[step],
                                                             self.buffer[agent_id].obs[step],
+                                                            self.buffer[agent_id].rnn_states[step],
+                                                            self.buffer[agent_id].rnn_states_critic[step],
+                                                            self.buffer[agent_id].masks[step])
+            # [agents, envs, dim]
+            values.append(_t2n(value))
+            action = _t2n(action)
+            # rearrange action
+            if self.envs.action_space[agent_id].__class__.__name__ == 'MultiDiscrete':
+                for i in range(self.envs.action_space[agent_id].shape):
+                    uc_action_env = np.eye(self.envs.action_space[agent_id].high[i]+1)[action[:, i]]
+                    if i == 0:
+                        action_env = uc_action_env
+                    else:
+                        action_env = np.concatenate((action_env, uc_action_env), axis=1)
+            elif self.envs.action_space[agent_id].__class__.__name__ == 'Discrete':
+                action_env = np.squeeze(np.eye(self.envs.action_space[agent_id].n)[action], 1)
+            else:
+                raise NotImplementedError
+
+            actions.append(action)
+            temp_actions_env.append(action_env)
+            action_log_probs.append(_t2n(action_log_prob))
+            rnn_states.append(_t2n(rnn_state))
+            rnn_states_critic.append( _t2n(rnn_state_critic))
+
+        # [envs, agents, dim]
+        actions_env = []
+        for i in range(self.n_rollout_threads):
+            one_hot_action_env = []
+            for temp_action_env in temp_actions_env:
+                one_hot_action_env.append(temp_action_env[i])
+            actions_env.append(one_hot_action_env)
+
+        values = np.array(values).transpose(1, 0, 2)
+        actions = np.array(actions).transpose(1, 0, 2)
+        action_log_probs = np.array(action_log_probs).transpose(1, 0, 2)
+        rnn_states = np.array(rnn_states).transpose(1, 0, 2, 3)
+        rnn_states_critic = np.array(rnn_states_critic).transpose(1, 0, 2, 3)
+
+        return values, actions, action_log_probs, rnn_states, rnn_states_critic, actions_env
+    
+    @torch.no_grad()
+    def collect_noise(self, step):
+        values = []
+        actions = []
+        temp_actions_env = []
+        action_log_probs = []
+        rnn_states = []
+        rnn_states_critic = []
+
+        for agent_id in range(self.num_agents):
+            self.trainer[agent_id].prep_rollout()
+            share_obs_noise = self.addNoise(self.buffer[agent_id].share_obs[step])
+            obs_noise = self.addNoise(self.buffer[agent_id].obs[step])
+            value, action, action_log_prob, rnn_state, rnn_state_critic \
+                = self.trainer[agent_id].policy.get_actions(share_obs_noise,
+                                                            obs_noise,
                                                             self.buffer[agent_id].rnn_states[step],
                                                             self.buffer[agent_id].rnn_states_critic[step],
                                                             self.buffer[agent_id].masks[step])
@@ -188,6 +323,67 @@ class MPERunner(Runner):
             for agent_id in range(self.num_agents):
                 self.trainer[agent_id].prep_rollout()
                 eval_action, eval_rnn_state = self.trainer[agent_id].policy.act(np.array(list(eval_obs[:, agent_id])),
+                                                                                eval_rnn_states[:, agent_id],
+                                                                                eval_masks[:, agent_id],
+                                                                                deterministic=True)
+
+                eval_action = eval_action.detach().cpu().numpy()
+                # rearrange action
+                if self.eval_envs.action_space[agent_id].__class__.__name__ == 'MultiDiscrete':
+                    for i in range(self.eval_envs.action_space[agent_id].shape):
+                        eval_uc_action_env = np.eye(self.eval_envs.action_space[agent_id].high[i]+1)[eval_action[:, i]]
+                        if i == 0:
+                            eval_action_env = eval_uc_action_env
+                        else:
+                            eval_action_env = np.concatenate((eval_action_env, eval_uc_action_env), axis=1)
+                elif self.eval_envs.action_space[agent_id].__class__.__name__ == 'Discrete':
+                    eval_action_env = np.squeeze(np.eye(self.eval_envs.action_space[agent_id].n)[eval_action], 1)
+                else:
+                    raise NotImplementedError
+
+                eval_temp_actions_env.append(eval_action_env)
+                eval_rnn_states[:, agent_id] = _t2n(eval_rnn_state)
+                
+            # [envs, agents, dim]
+            eval_actions_env = []
+            for i in range(self.n_eval_rollout_threads):
+                eval_one_hot_action_env = []
+                for eval_temp_action_env in eval_temp_actions_env:
+                    eval_one_hot_action_env.append(eval_temp_action_env[i])
+                eval_actions_env.append(eval_one_hot_action_env)
+
+            # Obser reward and next obs
+            eval_obs, eval_rewards, eval_dones, eval_infos = self.eval_envs.step(eval_actions_env)
+            eval_episode_rewards.append(eval_rewards)
+
+            eval_rnn_states[eval_dones == True] = np.zeros(((eval_dones == True).sum(), self.recurrent_N, self.hidden_size), dtype=np.float32)
+            eval_masks = np.ones((self.n_eval_rollout_threads, self.num_agents, 1), dtype=np.float32)
+            eval_masks[eval_dones == True] = np.zeros(((eval_dones == True).sum(), 1), dtype=np.float32)
+
+        eval_episode_rewards = np.array(eval_episode_rewards)
+        
+        eval_train_infos = []
+        for agent_id in range(self.num_agents):
+            eval_average_episode_rewards = np.mean(np.sum(eval_episode_rewards[:, :, agent_id], axis=0))
+            eval_train_infos.append({'eval_average_episode_rewards': eval_average_episode_rewards})
+            print("eval average episode rewards of agent%i: " % agent_id + str(eval_average_episode_rewards))
+
+        self.log_train(eval_train_infos, total_num_steps)  
+
+    @torch.no_grad()
+    def eval_noise(self, total_num_steps):
+        eval_episode_rewards = []
+        eval_obs = self.eval_envs.reset()
+
+        eval_rnn_states = np.zeros((self.n_eval_rollout_threads, self.num_agents, self.recurrent_N, self.hidden_size), dtype=np.float32)
+        eval_masks = np.ones((self.n_eval_rollout_threads, self.num_agents, 1), dtype=np.float32)
+
+        for eval_step in range(self.episode_length):
+            eval_temp_actions_env = []
+            for agent_id in range(self.num_agents):
+                self.trainer[agent_id].prep_rollout()
+                noise_obs = self.addNosie(np.array(list(eval_obs[:, agent_id])))
+                eval_action, eval_rnn_state = self.trainer[agent_id].policy.act(noise_obs,
                                                                                 eval_rnn_states[:, agent_id],
                                                                                 eval_masks[:, agent_id],
                                                                                 deterministic=True)
